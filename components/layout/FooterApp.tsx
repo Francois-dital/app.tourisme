5227c4ec0db94425fd35562ccf37bd55573f22c0
YaroRael <aroraelsl@gmail.com>
2026-01-31 07:39:56 +0100

    social link modified

--- components/layout/FooterApp.tsx
+++ components/layout/FooterApp.tsx
@@ -58,17 +58,26 @@ export default function FooterApp() {
               </div>
               
               <div className="flex items-center gap-4">
-                {socialLinks.map((social, index) => (
-                  <ScrollAnimation key={social.name} animation="scale" delay={200 + (index * 100)}>
-                    <Link
-                      href={social.href}
-                      className="size-10 rounded-full border border-white/10 flex items-center justify-center hover:bg-primary hover:text-background-dark hover:scale-110 transition-all"
-                      aria-label={social.name}
+                {/* Lien Facebook avec SVG direct */}
+                <ScrollAnimation animation="scale" delay={200}>
+                  <Link
+                    href="https://www.facebook.com/61587278189321"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="size-10 rounded-full border border-white/10 flex items-center justify-center hover:bg-[#1877F2] hover:text-white hover:scale-110 transition-all"
+                    aria-label="Facebook"
+                  >
+                    {/* SVG Facebook direct */}
+                    <svg 
+                      className="w-5 h-5" 
+                      fill="currentColor" 
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <Icon name={social.icon} />
-                    </Link>
-                  </ScrollAnimation>
-                ))}
+                      <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                    </svg>
+                  </Link>
+                </ScrollAnimation>
               </div>
             </div>
           </ScrollAnimation>
